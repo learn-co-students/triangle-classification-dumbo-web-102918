f887ed90cac79cfe677c639882f0abfc41995b7b
github username <github email address>
2018-11-06 09:46:00 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :side1, :side2, :side3
+
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if self.side1 * self.side2 * self.side3 = self.side1 ** 3
+      return :equilateral
+    end
+  
